72322dea95fcddcf4b9fd731c9de68c2b50d28f3
Devon Hansen <dhansen@lcs.on.ca>
2021-10-14 08:53:28 -0400

    Initial Commit

---
+++ Fun Facts JSON API/Fun_Facts_JSON_APIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fun_Facts_JSON_APIApp.swift
+//  Fun Facts JSON API
+//
+//  Created by Devon Kenneth Hansen on 2021-10-14.
+//
+
+import SwiftUI
+
+@main
+struct Fun_Facts_JSON_APIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
