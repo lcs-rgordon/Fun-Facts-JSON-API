7d00973515ec61cb07f3274df8a0c0f7afb58ca9
Devon Hansen <dhansen@lcs.on.ca>
2021-10-15 12:17:05 -0400

    fixed api link

--- Fun Facts JSON API/Base/Constants/APIConstants.swift
+++ Fun Facts JSON API/Base/Constants/APIConstants.swift
@@ -9,5 +9,5 @@ import Foundation
 
 
 enum APIConstants {
-    static let baseUrl = "https://asli-fun-fact-api.herokuapp.com/"
+    static let baseUrl = "https://asli-fun-fact-api.herokuapp.com"
 }
--- Fun Facts JSON API/Feature/Facts/Service/FactsService.swift
+++ Fun Facts JSON API/Feature/Facts/Service/FactsService.swift
@@ -6,16 +6,18 @@
 //
 
 import Foundation
+import SwiftUI
 
 protocol FactsService {
     func fetchRandomFacts() async throws -> [Fact]
 }
 
+
 final class FactsServiceImpl: FactsService {
     
     func fetchRandomFacts() async throws -> [Fact] {
         let urlSession = URLSession.shared
-        let url = URL(string: APIConstants.baseUrl.appending("/api/facts"))
+        let url = URL(string: APIConstants.baseUrl.appending("/"))
         let (data, _) = try await urlSession.data(from: url!)
         return try JSONDecoder().decode([Fact].self, from: data)
     }
